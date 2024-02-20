40197e3a9302e29e439b4b8c3d5e8db39b589931
Oliver Davies <oliver@oliverdavies.dev>
2024-02-20 12:21:55 +0000

    Fix PHPStan error

--- src/Process/Process.php
+++ src/Process/Process.php
@@ -8,13 +8,14 @@ use Symfony\Component\Process\Process as SymfonyProcess;
 
 /**
  * Create a new Process instance with some extra options.
- *
- * @param non-empty-array<int, non-empty-string> $command
- * @param string $workingDir
- * @param ?string $extraArgs
  */
 final class Process
 {
+    /**
+     * @param non-empty-array<int, non-empty-string> $command
+     * @param string $workingDir
+     * @param ?string $extraArgs
+     */
     public static function create(array $command, string $workingDir, ?string $extraArgs = null): SymfonyProcess
     {
         $process = new SymfonyProcess(command: array_filter([...$command, $extraArgs]));
